85ae9e4350fd22480bc3299708c65fcedb317f72
ayaanshakeelkhan12-ctrl <ayaanshakeelkhan12@gmail.com>
2026-04-16 08:49:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # AIM: Write a Python program to print a 
 # triangle pattern (give any), emphasizing
 # the transition from C to Python syntax.
-# Coder:
-# Date:
+# Coder: Khan Ayaan Shakeel
+# Date:4/2/26
 
 print("--- Pattern Printer ---\n")
 
 
 # Write your code here
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
